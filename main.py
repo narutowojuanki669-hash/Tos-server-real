d0405a93a77ff7311db02a270d9ec397ee5d72e7
narutowojuanki669-hash <narutowojuanki669@gmail.com>
2025-11-05 01:22:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
-origins = [
-    "https://690a4beb4e174c424fe59d8c--townofshadows.netlify.app",
-    "http://localhost:3000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://690a4beb4e174c424fe59d8c--townofshadows.netlify.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/test")
-def read_root():
-    return {"message": "Hello from Town of Shadows backend!"}
